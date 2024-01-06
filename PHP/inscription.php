a6225590ec95d0e07d2a7a324b50ecdfc9744105
AmalizzyDHY <amalizzy.dhy@gmail.com>
2024-01-06 21:34:29 +0100

    inscription vérification de si le mail existe déjà

--- PHP/inscription.php
+++ PHP/inscription.php
@@ -50,25 +50,41 @@
         <main>
 
             <?php
-            include "../DB/database.php";
-            $conn = dbConnect();
+              include "../DB/database.php";
+              $conn = dbConnect();
 
-            if(!empty($_POST['Nom']) && !empty($_POST['Prenom']) && !empty($_POST['Telephone']) && !empty($_POST['Mail']) && !empty($_POST['Mdp'])){
-              if($_POST['Mail'] == $_POST['Mail_verif'] && $_POST['Mdp'] == $_POST['Mdp_verif']){
-                $request = $conn->prepare("INSERT INTO utilisateur (nom, prenom, telephone, adresse_email, mot_de_passe) VALUES (:nom, :prenom, :telephone, :adresse_email, :mot_de_passe)");
-                $request->bindParam(':nom', $_POST['Nom']);
-                $request->bindParam(':prenom', $_POST['Prenom']);
-                $request->bindParam(':telephone', $_POST['Telephone']);
-                $request->bindParam(':adresse_email', $_POST['Mail']);
-                $request->bindParam(':mot_de_passe', $_POST['Mdp']);
-                $request->execute();
-                $_POST['Nom'] = "";
-                $_POST['Prenom'] = "";
-                $_POST['Telephone'] = "";
-                $_POST['Mail'] = "";
-                $_POST['Mdp'] = "";
+              if(!empty($_POST['Nom']) && !empty($_POST['Prenom']) && !empty($_POST['Telephone']) && !empty($_POST['Mail']) && !empty($_POST['Mdp'])){
+                if($_POST['Mail'] == $_POST['Mail_verif'] && $_POST['Mdp'] == $_POST['Mdp_verif']){
+
+                  $verification = $conn->prepare('SELECT telephone FROM utilisateur WHERE adresse_email = :email');
+                  $verification->bindParam(':email', $_POST['Mail']);
+                  $verification->execute();
+                  $tel_email = $verification->fetchAll()[0][0]; 
+
+                  if ( $_POST['Telephone'] != $tel_email ){
+                    //dire que l'adresse mail est déjà utiliser
+                    echo '<script language="Javascript">
+                    alert("Cet adresse mail est déjà utiliser par un autre compte")
+                    </script>';
+                  }
+                  if ( $tel_email == null ){
+                    //laisser le gens créer son compte
+                  }
+
+                  $request = $conn->prepare("INSERT INTO utilisateur (nom, prenom, telephone, adresse_email, mot_de_passe) VALUES (:nom, :prenom, :telephone, :adresse_email, :mot_de_passe)");
+                  $request->bindParam(':nom', $_POST['Nom']);
+                  $request->bindParam(':prenom', $_POST['Prenom']);
+                  $request->bindParam(':telephone', $_POST['Telephone']);
+                  $request->bindParam(':adresse_email', $_POST['Mail']);
+                  $request->bindParam(':mot_de_passe', $_POST['Mdp']);
+                  $request->execute();
+                  $_POST['Nom'] = "";
+                  $_POST['Prenom'] = "";
+                  $_POST['Telephone'] = "";
+                  $_POST['Mail'] = "";
+                  $_POST['Mdp'] = "";
+                }
               }
-            }
             ?>
 
             <div id="form_insciption">
