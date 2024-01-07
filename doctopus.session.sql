8f8708a2b79746eff7b676ce71e5545348623a61
AmalizzyDHY <amalizzy.dhy@gmail.com>
2024-01-07 16:37:47 +0100

    rendez vous passee

--- doctopus.session.sql
+++ doctopus.session.sql
@@ -1,1 +1,5 @@
-SELECT * FROM utilisateur;
+SELECT * FROM utilisateur u, reserver r, rendez_vous rv
+WHERE u.adresse_email = r.adresse_email
+AND r.id_rendez_vous = rv.id_rendez_vous;
+
+SELECT * FROM medecin;
