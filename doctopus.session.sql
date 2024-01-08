6306158da647a6a1e679b2bea5e5bff10326d710
Balblaa <vdronne86@orange.fr>
2024-01-08 18:01:28 +0100

    le dernier

--- doctopus.session.sql
+++
@@ -1,6 +0,0 @@
-SELECT r.adresse_email, rv.date, rv.heure, m.nom, m.specialite, m.localisation
-FROM medecin m, reserver r, rendez_vous rv
-WHERE r.id_rendez_vous = rv.id_rendez_vous
-AND m.id_medecin = rv.id_medecin ;
-AND 
-ORDER BY rv.date, rv.heure, m.nom ;
