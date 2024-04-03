2e510f6ede98c0cc004b12b56df4bb78404ed4d9
Balblaa <vdronne86@orange.fr>
2024-04-03 17:25:20 +0200

    request getspe

--- doctopus.session.sql
+++ doctopus.session.sql
@@ -1,3 +1,1 @@
-UPDATE rendez_vous SET disponible = '0' WHERE id_rendez_vous = 6;
-INSERT INTO reserver (id_rendez_vous, adresse_email) VALUES (6 , 'valentin.dronne@isen-ouest.yncrea.fr');
-SELECT * FROM rendez_vous;
+SELECT DISTINCT localisation FROM medecin ORDER BY localisation ASC;
